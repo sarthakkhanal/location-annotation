936b03cf60454638caeddbba2ff118ebb7519856
sarthakkhanal <sarthakkhanal2016@gmail.com>
2019-04-02 11:52:42 -0500

    Update Relevance.php

--- app/Models/Location/Relevance.php
+++ app/Models/Location/Relevance.php
@@ -7,7 +7,7 @@ use Illuminate\Database\Eloquent\Model;
 class Relevance extends Model
 {
   protected $table="location_tweet_relevance";
-  protected $fillable = ['user_id','tweet_id','relevance',"humanitarian","eye_witness"];
+  protected $fillable = ['user_id','tweet_id','relevance'];
   public $timestamps=false;
   public function tweet(){
     return $this->belongsTo("App\Models\Location\Tweets","tweet_id","id");

c0a442f1a252b3becc54383cb69926288bdb86c4
sarthakkhanal <sarthakkhanal2016@gmail.com>
2019-04-02 11:14:32 -0500

    location.php
    Config file that contains the list of categories used to annotate location mentions

---
+++ config/location.php
@@ -0,0 +1,31 @@
+<?php
+return [
+  "categories"=>[
+    "Street address",
+    "State",
+    "Territory",
+    "Country",
+    "Neighborhood/City/Town",
+    "Landmark/Building",
+    "Location within building",
+    "Intersection",
+    "Moving target",
+    "Coordinates",
+    "Region",
+    "Continent",
+    "Space locations",
+    "Other"
+  ],
+  "humanitarian"=>[
+    "Affected individuals",
+    "Infrastructure and utility damage",
+    "Injured or dead people",
+    "Missing or found people",
+    "Rescue, volunteering or donation effort",
+    "Vehicle damage",
+    "None"
+  ]
+
+]
+
+ ?>

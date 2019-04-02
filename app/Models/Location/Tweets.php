0f419c9dded8c0a779f43a734811f09da17f6f49
sarthakkhanal <sarthakkhanal2016@gmail.com>
2019-04-02 11:30:25 -0500

    Tweets model
    Model used to store tweet data

---
+++ app/Models/Location/Tweets.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace App\Models\Location;
+
+use Illuminate\Database\Eloquent\Model;
+use App\Models\Location\LocationLabels;
+use App\Models\Location\Relevance;
+use Auth;
+class Tweets extends Model
+{
+  protected $table="location_tweets";
+  protected $primaryKey = 'id';
+  public $incrementing = false;
+  protected $with =['locationLabels','locationRelevance'];
+  public $timestamps=false;
+  public function locationLabels(){
+    return $this->hasMany(LocationLabels::class,'tweet_id','id')->where('user_id',Auth::user()->id);
+  }
+  public function locationRelevance(){
+    return $this->hasOne(Relevance::class,'tweet_id','id')->where('user_id',Auth::user()->id);
+  }
+  public function syncLocationLabels($data){
+    $this->locationLabels()->delete();
+    $relevance=0;
+    if(count($data["labels"])>0){
+      $relevance=1;
+    }
+    $relevance=Relevance::updateOrCreate([
+      "tweet_id"=>$this->id,
+      "user_id"=>Auth::user()->id
+    ],[
+      "relevance"=>$relevance,
+      "humanitarian"=>$data["humanitarian"],
+      "eye_witness"=>$data["eye_witness"]
+    ]);
+
+    foreach($data["labels"] as $datum){
+      $this->locationLabels()->save(new LocationLabels($datum));
+    }
+  }
+  public function prepareForForm(){
+    $data=[];
+    foreach($this->locationLabels as $labels){
+      $data[]=[
+        "location"=>$labels->location,
+        "location_type"=>$labels->type
+      ];
+    }
+    return $data;
+  }
+}
